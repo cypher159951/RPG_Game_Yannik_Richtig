1688a01be0b606fc46d70d5a52115840af3bab36
yannikbleistein <yannikbleistein@gmx.de>
2023-07-13 16:42:23 +0200

    13.07.2023 16:42 Uhr

--- src/main/kotlin/KleinerHelferJohn.kt
+++ src/main/kotlin/KleinerHelferJohn.kt
@@ -2,10 +2,25 @@ import kotlin.random.Random
 
 class KleinerHelferJohn(name: String, leben: Int) : Boss(name, leben) {
 
+    val attacken = listOf("schwaechung")
+
     fun schwaechung(held: Held) {
         val schaden = Random.nextInt(100, 150)
         held.schaden(schaden)
         println("${name} führt die Attacke Schwächung aus und verursacht $schaden Schaden bei ${held.name}")
     }
-}
 
+    fun heilung(boss: Boss) {                                                                          //Funktion wird nicht benutzt
+        val heilung = Random.nextInt(400, 900)
+        lebenspunkte += heilung
+        println("${name} heilt sich um ${heilung} und hat nun wieder ${lebenspunkte} Lebenspunkte")
+    }
+
+
+    override fun angreifen(held: Held, helden: List<Held>) {
+        val zufaelligeAttacke = attacken.random()
+        when (zufaelligeAttacke) {
+            "gifthieb" -> schwaechung(held)
+        }
+    }
+}
